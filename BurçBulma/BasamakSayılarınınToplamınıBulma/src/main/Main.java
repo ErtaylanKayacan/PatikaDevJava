25a86be872e729c4d1c2418392691f3a98f572f4
ErtaylanKayacan <101421107+ErtaylanKayacan@users.noreply.github.com>
2023-01-25 16:46:51 +0300

    Add files via upload

---
+++ BurçBulma/BasamakSayılarınınToplamınıBulma/src/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz :");
+        int number = input.nextInt();
+        int basNumber = 0;
+        int tempNumber = number;
+        int basValue;
+        int result = 0;
+        
+        while (tempNumber != 0) {
+            tempNumber /= 10;
+            basNumber++;
+        }
+
+        tempNumber = number;
+        while (tempNumber != 0) {
+            basValue = tempNumber % 10;
+            result += basValue;
+            tempNumber /= 10;
+        }
+        System.out.println(result);
+	}
+
+}
